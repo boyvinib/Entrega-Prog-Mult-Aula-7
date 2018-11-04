08cf5f66e4868006cee5786466ec098a8be796fd
boyvinib <b_viniciusb@hotmail.com>
2018-11-04 16:21:56 -0200

    Entrega-Prog-Mult-Aula-7

---
+++ entrega_prog_multi_aula07/src/model/model_filme.ts
@@ -0,0 +1,13 @@
+/**
+ *  Nome: Vinicius Ferreira Silva RA: 817127791
+ */
+
+export interface Filme {
+    nome: string;
+    descricao: string;
+}
+
+export interface Genero {
+    nome: string;
+    genero: string;
+}
---
+++ entrega_prog_multi_aula07/src/pages/crud/crud.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+//import { TabsPage } from '../tabs/tabs';
+import { ContactPage } from '../contact/contact';
+import { FilmePage } from '../filme/filme';
+
+/**
+ * Generated class for the CrudPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ * 
+ *  Nome: Vinicius Ferreira Silva RA: 817127791
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-crud',
+  templateUrl: 'crud.html',
+  
+})
+export class CrudPage {
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CrudPage');
+  }
+
+  goToTabsFilme (){
+    this.navCtrl.push(FilmePage);
+  }
+
+}
